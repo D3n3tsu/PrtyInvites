063ed8e94b8920c3fc30e58ecc23d6bff856ba33
null-n-void <ddm_09@ukr.net>
2017-03-31 23:58:06 +0300

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponce guestResponce)
+        {
+            //Todo: Email responce to the party organizers
+            return View("Thanks", guestResponce);
+        }
+    }
+}
